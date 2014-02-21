842a9df8d7cd0d432041712104ef6fc7b468c4bf
Eric Sandall <sandalle@gmail.com>
2014-02-20 16:55:53 -0800

    Add requirements and better comments to startup program.

--- lolmer_bigreactor_startup.lua
+++ lolmer_bigreactor_startup.lua
@@ -8,6 +8,14 @@
 	in Minecraft with a Computercraft computer, using Computercraft's
 	own wired modem connected to the reactors computer control port.
 
+	Save this file as "startup" on your computer for it to auto-start on Computer boot.
+
+	Requirements:
+		Advanced Monitor size is X: 29, Y: 12 with a 3x2 size
+		Computer or Advanced Computer
+		Modems (not wireless) connecting each of the Computer to both the Advanced Monitor and Reactor Computer Port.
+
+
 	This script is available from:
 		http://pastebin.com/ZTMzRLez
 		https://github.com/sandalle/minecraft_bigreactor_control
@@ -25,10 +33,14 @@
 ]]--
 os.setComputerLabel("ReactorControl")
 
+-- Remove any prior, possibly old, versions of "reactorcontrol"
 shell.run("rm", "reactorcontrol")
+-- Download http://pastebin.com/fguScPBQ and name "reactorcontrol"
 shell.run("pastebin", "get", "fguScPBQ", "reactorcontrol")
 
+-- Remove any prior, possibly old, versions of "FC_API"
 shell.run("rm", "FC_API")
+-- Download http://pastebin.com/A9hcbZWe and name "FC_API"
 shell.run("pastebin", "get", "A9hcbZWe", "FC_API")
 
 shell.run("reactorcontrol")
