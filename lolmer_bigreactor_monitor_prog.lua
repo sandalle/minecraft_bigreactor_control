7899fc17d58b11a2b38ba364596edd66fedeb8e6
Eric Sandall <sandalle@gmail.com>
2014-02-26 16:52:27 -0800

    Un-tab the description comment

--- lolmer_bigreactor_monitor_prog.lua
+++ lolmer_bigreactor_monitor_prog.lua
@@ -1,120 +1,120 @@
 --[[
-	Program name: Lolmer's EZ-NUKE reactor control system
-	Version: v0.3.3
-	Programmer: Lolmer
-	Last update: 2014-02-25
-	Pastebin: http://pastebin.com/fguScPBQ
+Program name: Lolmer's EZ-NUKE reactor control system
+Version: v0.3.3
+Programmer: Lolmer
+Last update: 2014-02-25
+Pastebin: http://pastebin.com/fguScPBQ
 
-	Description:
-	This program controls a Big Reactors nuclear reactor
-	in Minecraft with a Computercraft computer, using Computercraft's
-	own wired modem connected to the reactors computer control port.
+Description:
+This program controls a Big Reactors nuclear reactor
+in Minecraft with a Computercraft computer, using Computercraft's
+own wired modem connected to the reactors computer control port.
 
-	This program was designed to work with the mods and versions installed on Never Stop Toasting (NST) Diet
-	http://www.technicpack.net/modpack/details/never-stop-toasting-diet.254882
-	Endeavour: Never Stop Toasting: Diet official Minecraft server http://forums.somethingawful.com/showthread.php?threadid=3603757
+This program was designed to work with the mods and versions installed on Never Stop Toasting (NST) Diet
+http://www.technicpack.net/modpack/details/never-stop-toasting-diet.254882
+Endeavour: Never Stop Toasting: Diet official Minecraft server http://forums.somethingawful.com/showthread.php?threadid=3603757
 
-	To simplify the code and guesswork, I assume the following monitor layout:
-	1) One reactor with one or more Advanced Monitors (all monitors display the same info).
-		OR
-	2) One Advanced Monitor for overall status display plus (first found monitor)
-	   one Advanced Monitor for each connected Reactor plus (subsequent found monitors)
-	   one Advanced Monitor for each connected Turbine (last group of monitors found).
-	If you enable debug mode, add one additional Advanced Monitor for #1 or #2.
+To simplify the code and guesswork, I assume the following monitor layout:
+1) One reactor with one or more Advanced Monitors (all monitors display the same info).
+	OR
+2) One Advanced Monitor for overall status display plus (first found monitor)
+   one Advanced Monitor for each connected Reactor plus (subsequent found monitors)
+   one Advanced Monitor for each connected Turbine (last group of monitors found).
+If you enable debug mode, add one additional Advanced Monitor for #1 or #2.
 
-	When using actively cooled reactors with turbines, keep the following in mind:
-		- 1 mB steam carries up to 10RF of potential energy to extract in a turbine.
-		- Actively cooled reactors produce steam, not power.
+When using actively cooled reactors with turbines, keep the following in mind:
+	- 1 mB steam carries up to 10RF of potential energy to extract in a turbine.
+	- Actively cooled reactors produce steam, not power.
 
-	Features:
-		Configurable min/max energy buffer and min/max temperature via ReactorOptions file.
-		ReactorOptions is read on start and then current values are saved every program cycle.
-		Rod Control value in ReactorOptions is only useful for initial start, after that the program saves the current Rod Control average over all Fuel Rods for next boot.
-		Auto-adjusts control rods per reactor to maintain temperature.
-		Will display reactor data to all attached monitors of correct dimensions.
-			For multiple monitors, the first monitor (often last plugged in) is the overall status monitor.
-			Dynamically detect and add/remove monitors as they are connected to the network (not recommended).
-		Disable rod auto-adjust by right-clicking between the rod control buttons "<" and ">"
+Features:
+	Configurable min/max energy buffer and min/max temperature via ReactorOptions file.
+	ReactorOptions is read on start and then current values are saved every program cycle.
+	Rod Control value in ReactorOptions is only useful for initial start, after that the program saves the current Rod Control average over all Fuel Rods for next boot.
+	Auto-adjusts control rods per reactor to maintain temperature.
+	Will display reactor data to all attached monitors of correct dimensions.
 		For multiple monitors, the first monitor (often last plugged in) is the overall status monitor.
+		Dynamically detect and add/remove monitors as they are connected to the network (not recommended).
+	Disable rod auto-adjust by right-clicking between the rod control buttons "<" and ">"
+	For multiple monitors, the first monitor (often last plugged in) is the overall status monitor.
 
-	Default values:
-		Rod Control: 90% (Let's start off safe and then power up as we can)
-		Minimum Energy Buffer: 15% (will power on below this value)
-		Maximum Energy Buffer: 85% (will power off above this value)
-		Minimum Temperature: 850^C (will raise control rods below this value)
-		Maximum Temperature: 950^C (will lower control rods above this value)
+Default values:
+	Rod Control: 90% (Let's start off safe and then power up as we can)
+	Minimum Energy Buffer: 15% (will power on below this value)
+	Maximum Energy Buffer: 85% (will power off above this value)
+	Minimum Temperature: 850^C (will raise control rods below this value)
+	Maximum Temperature: 950^C (will lower control rods above this value)
 
-	Requirements:
-		Advanced Monitor size is X: 29, Y: 12 with a 3x2 size
-		Computer or Advanced Computer
-		Modems (not wireless) connecting each of the Computer to both the Advanced Monitor and Reactor Computer Port.
-		Big Reactors (http://www.big-reactors.com/) 0.3
-		Computercraft (http://computercraft.info/) 1.57+
+Requirements:
+	Advanced Monitor size is X: 29, Y: 12 with a 3x2 size
+	Computer or Advanced Computer
+	Modems (not wireless) connecting each of the Computer to both the Advanced Monitor and Reactor Computer Port.
+	Big Reactors (http://www.big-reactors.com/) 0.3
+	Computercraft (http://computercraft.info/) 1.57+
 
-	Resources:
-	This script is available from:
-		http://pastebin.com/fguScPBQ
-		https://github.com/sandalle/minecraft_bigreactor_control
-	Start-up script is available from:
-		http://pastebin.com/ZTMzRLez
-		https://github.com/sandalle/minecraft_bigreactor_control
-	Other reactor control program which I based my program on:
-		http://pastebin.com/aMAu4X5J (ScatmanJohn)
-		http://pastebin.com/HjUVNDau (version ScatmanJohn based his on)
-	A simpler Big Reactor control program is available from:
-		http://pastebin.com/tFkhQLYn (IronClaymore)
+Resources:
+This script is available from:
+	http://pastebin.com/fguScPBQ
+	https://github.com/sandalle/minecraft_bigreactor_control
+Start-up script is available from:
+	http://pastebin.com/ZTMzRLez
+	https://github.com/sandalle/minecraft_bigreactor_control
+Other reactor control program which I based my program on:
+	http://pastebin.com/aMAu4X5J (ScatmanJohn)
+	http://pastebin.com/HjUVNDau (version ScatmanJohn based his on)
+A simpler Big Reactor control program is available from:
+	http://pastebin.com/tFkhQLYn (IronClaymore)
 
-	Reactor Computer Port API: http://wiki.technicpack.net/Reactor_Computer_Port
-	Computercraft API: http://computercraft.info/wiki/Category:APIs
-	Big Reactors Efficiency, Speculation and Questions! http://www.reddit.com/r/feedthebeast/comments/1vzds0/big_reactors_efficiency_speculation_and_questions/
-	Big Reactors API code: https://github.com/erogenousbeef/BigReactors/blob/master/erogenousbeef/bigreactors/common/multiblock/tileentity/TileEntityReactorComputerPort.java
-	Big Reactors API: http://big-reactors.com/cc_api.html
+Reactor Computer Port API: http://wiki.technicpack.net/Reactor_Computer_Port
+Computercraft API: http://computercraft.info/wiki/Category:APIs
+Big Reactors Efficiency, Speculation and Questions! http://www.reddit.com/r/feedthebeast/comments/1vzds0/big_reactors_efficiency_speculation_and_questions/
+Big Reactors API code: https://github.com/erogenousbeef/BigReactors/blob/master/erogenousbeef/bigreactors/common/multiblock/tileentity/TileEntityReactorComputerPort.java
+Big Reactors API: http://big-reactors.com/cc_api.html
 
-	ChangeLog:
-	0.3.3 - Add Big Reactor Turbine support
-		First found monitor (appears to be last connected monitor) is used to display status of all found devices (if more than one valid monitor is found)
-		Display monitor number i top left of each monitor as "M#" to help find which monitor is which.
-		Enabling debug will use the last monitor found, if more than one, to print out debug info (also written to file)
-		Add monitor layout requirements to simplify code
-		Only clear monitors when we're about to use them (e.g. turbine monitors no longer clear, then wait for all reactors to update)
-		Fix getDeviceStoredEnergyBufferPercent(), was off by a decimal place
-		Just use first Control Rod level for entire reactor, they are no longer treated individually in BR 0.3
-	0.3.2 - Allow for rod control to override (disable) auto-adjust via UI (Rhonyn)
-	0.3.1 - Add fuel consumption per tick to display
-	0.3.0 - Add multi-monitor support! Sends one reactor's data to all monitors.
-		print function now takes table to support optional specified monitor
-		Set "numRods" every cycle for some people (mechaet)
-		Don't redirect terminal output with multiple monitor support
-		Log troubleshooting data to reactorcontrol.log
-		FC_API no longer used (copied and modified what I needed)
-		Multi-reactor support is theoretically implemented, but it is UNTESTED!
-		Updated for Big Reactor 0.3 (no longer works with 0.2)
-		BR getFuelTemperature() now returns many significant digits, just use math.ceil()
-		BR 0.3 removed individual rod temperatures, now it's only reactor-level temperature
-	0.2.4 - Simplify math, don't divide by a simple large number and then multiply by 100 (#/10000000*100)
-		Fix direct-connected (no modem) devices. getDeviceSide -> FC_API.getDeviceSide (simple as that :))
-	0.2.3 - Check bounds on reactor.setRodControlLevel(#,#), Big Reactor doesn't check for us.
-	0.2.2 - Do not auto-start the reactor if it was manually powered off (autoStart=false)
-	0.2.1 - Lower/raise only the hottest/coldest Control Rod while trying to control the reactor temperature.
-		"<" Rod Control buttons was off by one (to the left)
-	0.2.0 - Lolmer Edition :)
-		Add min/max stored energy percentage (default is 15%/85%), configurable via ReactorOptions file.
-		No reason to keep burning fuel if our power output is going nowhere. :)
-		Use variables variable for the title and version.
-		Try to keep the temperature between configured values (default is 850^C-950^C)
-		Add Waste and number of Control/Fuel Rods to displayBards()
+ChangeLog:
+0.3.3 - Add Big Reactor Turbine support
+	First found monitor (appears to be last connected monitor) is used to display status of all found devices (if more than one valid monitor is found)
+	Display monitor number i top left of each monitor as "M#" to help find which monitor is which.
+	Enabling debug will use the last monitor found, if more than one, to print out debug info (also written to file)
+	Add monitor layout requirements to simplify code
+	Only clear monitors when we're about to use them (e.g. turbine monitors no longer clear, then wait for all reactors to update)
+	Fix getDeviceStoredEnergyBufferPercent(), was off by a decimal place
+	Just use first Control Rod level for entire reactor, they are no longer treated individually in BR 0.3
+0.3.2 - Allow for rod control to override (disable) auto-adjust via UI (Rhonyn)
+0.3.1 - Add fuel consumption per tick to display
+0.3.0 - Add multi-monitor support! Sends one reactor's data to all monitors.
+	print function now takes table to support optional specified monitor
+	Set "numRods" every cycle for some people (mechaet)
+	Don't redirect terminal output with multiple monitor support
+	Log troubleshooting data to reactorcontrol.log
+	FC_API no longer used (copied and modified what I needed)
+	Multi-reactor support is theoretically implemented, but it is UNTESTED!
+	Updated for Big Reactor 0.3 (no longer works with 0.2)
+	BR getFuelTemperature() now returns many significant digits, just use math.ceil()
+	BR 0.3 removed individual rod temperatures, now it's only reactor-level temperature
+0.2.4 - Simplify math, don't divide by a simple large number and then multiply by 100 (#/10000000*100)
+	Fix direct-connected (no modem) devices. getDeviceSide -> FC_API.getDeviceSide (simple as that :))
+0.2.3 - Check bounds on reactor.setRodControlLevel(#,#), Big Reactor doesn't check for us.
+0.2.2 - Do not auto-start the reactor if it was manually powered off (autoStart=false)
+0.2.1 - Lower/raise only the hottest/coldest Control Rod while trying to control the reactor temperature.
+	"<" Rod Control buttons was off by one (to the left)
+0.2.0 - Lolmer Edition :)
+	Add min/max stored energy percentage (default is 15%/85%), configurable via ReactorOptions file.
+	No reason to keep burning fuel if our power output is going nowhere. :)
+	Use variables variable for the title and version.
+	Try to keep the temperature between configured values (default is 850^C-950^C)
+	Add Waste and number of Control/Fuel Rods to displayBards()
 
-	TODO:
-		Support multiple reactors
-		- If multiple reactors, require a monitor for each reactor and display only that reactor on a monitor
-		- Save parameters per reactor instead of one global set for all reactors
-		- Allow for a monitor (first monitor would be good) to display compressed info from all reactors (Rhonyn)
-		- Needing (r+t)+1 monitors (# reactors + # turbines) + overall display for multi-reactor
-		Add min/max RF/t output and have it override temperature concerns (maybe?)
-		Add support for wireless modems, see http://computercraft.info/wiki/Modem_%28API%29, will not be secure (anyone can send/listen to your channels)!
-		Add support for any sized monitor (minimum 3x3), dynamic allocation/alignment
-		Add BR 0.3 active-cooled Turbine control support (see https://twitter.com/ErogenousBeef/status/437663302403891200/photo/1/large)
-		Add fuel reactivity to reactor display
+TODO:
+	Support multiple reactors
+	- If multiple reactors, require a monitor for each reactor and display only that reactor on a monitor
+	- Save parameters per reactor instead of one global set for all reactors
+	- Allow for a monitor (first monitor would be good) to display compressed info from all reactors (Rhonyn)
+	- Needing (r+t)+1 monitors (# reactors + # turbines) + overall display for multi-reactor
+	Add min/max RF/t output and have it override temperature concerns (maybe?)
+	Add support for wireless modems, see http://computercraft.info/wiki/Modem_%28API%29, will not be secure (anyone can send/listen to your channels)!
+	Add support for any sized monitor (minimum 3x3), dynamic allocation/alignment
+	Add BR 0.3 active-cooled Turbine control support (see https://twitter.com/ErogenousBeef/status/437663302403891200/photo/1/large)
+	Add fuel reactivity to reactor display
 
 ]]--
 
