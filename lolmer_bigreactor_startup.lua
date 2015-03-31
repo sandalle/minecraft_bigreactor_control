a14ea543044d78d7ae899323c6c5d57197b158af
Eric Sandall <sandalle@gmail.com>
2015-03-31 14:52:18 -0700

    Fiddling with switching from pastebin to gitget
    Trying to use gitget2 from http://www.computercraft.info/forums2/index.php?/topic/17387-gitget-version-2-release/ for pulling updates directly from GitHub instead of using pastebin, since I cannot seem to get a GitHub WebHook to submit releases directly to an existing pastebin ID.

--- lolmer_bigreactor_startup.lua
+++ lolmer_bigreactor_startup.lua
@@ -1,8 +1,8 @@
 --[[
 	Programmer: Lolmer
-	Last update: 2014-02-28
+	Last update: 2015-03-31
 	Pastebin: http://pastebin.com/ZTMzRLez
-	GitHub: https://github.com/sandalle/minecraft_bigreactor_control
+	GitHub: https://github.com/sandalle/minecraft_bigreactor_control/
 
 	Description:
 	This program controls a Big Reactors nuclear reactor
@@ -14,6 +14,8 @@
 	> rm startup
 	> lua
 	lua> shell.run("pastebin", "get", "ZTMzRLez", "startup")
+
+	Now uses gitget http://www.computercraft.info/forums2/index.php?/topic/17387-gitget-version-2-release/
 
 	Requirements:
 		Advanced Monitor size is X: 29, Y: 12 with a 3x2 size
@@ -29,7 +31,7 @@
 		http://pastebin.com/aMAu4X5J (ScatmanJohn)
 		http://pastebin.com/HjUVNDau (version ScatmanJohn based his on)
 	A simpler Big Reactor control is available from:
-		http://pastebin.com/tFkhQLYn (IronClaymore)
+		http://pastebin.com/7S5xCvgL (IronClaymore)
 
 	Advanced Monitor size is X: 29, Y: 12 with a 3x2 size
 ]]--
@@ -37,7 +39,20 @@ os.setComputerLabel("ReactorControl")
 
 -- Remove any prior, possibly old, versions of "reactorcontrol"
 shell.run("rm", "reactorcontrol")
--- Download http://pastebin.com/fguScPBQ and name "reactorcontrol"
-shell.run("pastebin", "get", "fguScPBQ", "reactorcontrol")
+shell.run("rm", "minecraft_bigreactor_control")
 
-shell.run("reactorcontrol")
+-- Install ElvishJerricco's JSON parsing API from http://pastebin.com/raw.php?i=4nRg9CHU
+if not fs.exists(json) then
+	shell.run("pastebin", "get", "4nRg9CHU", "gitget")
+end
+
+-- Install gitget2 http://www.computercraft.info/forums2/index.php?/topic/17387-gitget-version-2-release/page__st__20
+if fs.exists(gitget) then
+	shell.run("gitget", "sandalle", "minecraft_bigreactor_control")
+else -- Fallback to pastebin to retrieve gitget
+	-- Download http://pastebin.com/raw.php?i=W5ZkVYSi and name "gitget"
+	shell.run("pastebin", "get", "W5ZkVYSi", "gitget")
+	shell.run("gitget", "sandalle", "minecraft_bigreactor_control")
+end
+
+shell.run("minecraft_bigreactor_control")
